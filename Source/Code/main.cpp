f3e38e7455a4fc23c460c25851c2772d7b8215bf
Reverie Wisp <reveriewisp@gmail.com>
2016-07-06 04:11:08 -0700

    Main added

---
+++ Source/Code/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "RTest.hpp"
+
+
+
+int main(int argc, char** argv) try
+{
+  //Parse command line. This will have both song names and flags to be interpreted. Consider getopt or similar for cross-platform stuff.
+  //Parse config file. Just reinterpret_cast the data to the DJConfig structure.
+  //Construct DJ object. One-liner.
+  //Feed DJ songs from command line or folder that command line specifies (note: NOT DJ config, but existing logic)
+  //While loop for DJ updating //while(DJ.update()) or something.
+    //Input Handler Update -- probably offload input parsing to another function?
+    //Update DJ, which by extension gets everything.
+  return 0;
+}
+//Catch and dump any errors.
+catch (RTest::RException e)
+{
+  std::cerr << e << std::endl;
+  std::cout << e << std::endl;
+  throw e;
+}
