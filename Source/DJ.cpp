dc4f8a2aa7638f957ae12527334b0dc2fd7e9e5a
GHOST\rever <reveriewisp@gmail.com>
2016-10-23 14:48:17 -0700

    + Implemented JDJ

--- Source/DJ.cpp
+++ Source/DJ.cpp
@@ -3,11 +3,18 @@
 namespace ASCIIPlayer
 {
   // Constructor
-  DJ::DJ(DJConfig config)
-    : hasShutdown_(false)
+  DJ::DJ(DJConfig config, bool startPaused)
+    : playlist_(&DJ::playlistUpdatedCallback, this)
+    , audioSystem_(config.ChannelCount)
+    , visualizer_(nullptr)
+    , config_(config)
+    , hasShutdown_(false)
+    , currSong_(false)
+    , paused_(startPaused)
   {
-
+    //!TODO: Handle visualizer configuration
   }
+
 
   // Destructor, calls shutdown if user did not.
   DJ::~DJ()
@@ -19,32 +26,93 @@ namespace ASCIIPlayer
   // Member Functons
   bool DJ::Update()
   {
+    if (!hasShutdown_)
+    {
+      if (visualizer_)
+      {
+        //!TODO: Make this more efficient, don't allocate it every time.
+        float vals[a128];
+        FillSongSpectrum(vals, a128, FMOD_DSP_FFT_WINDOW_BLACKMAN); //blackman windooooowwww yaaaaaaaaaaassss (change that, rect is def)
+        visualizer_->Update(vals, a128);
+      }
 
+      // Update our audio system at the very end.
+      audioSystem_.Update();
+      return true;
+    }
+    else
+      return false;
   }
   
+
+  // Plays the current song, or starts if there's nothing.
+  void DJ::Play()
+  {
+    if (currSong_)
+    {
+      if (paused_)
+      {
+        audioSystem_.TogglePause(*currSong_);
+        paused_ = false;
+      }
+    }
+  }
+
+  void DJ::Pause()
+  {
+    if (currSong_)
+    {
+      if (!paused_)
+      {
+        audioSystem_.TogglePause(*currSong_);
+        paused_ = true;
+      }
+    }
+  }
+
   // Shuts down the DJ, closing out of all the audio that has been loaded and it has
   // prepped for.
   void DJ::Shutdown()
   {
-
     hasShutdown_ = true;
   }
 
+
+  // Shuffles all the songs in the playlist.
   void DJ::ShufflePlaylist()
   {
-
+    playlist_.Scramble();
   }
+
 
   // Adds a song to the DJ playlist.
-  // All songs must be in a playlist.
-  void DJ::AddSong(AudioFile toAdd)
+  // All songs must be in a playlist, even solo songs. They get added to the DJ playlist.
+  // This will be allocated when passed. new AudioFile("string");
+  // The DJ does NOT handle deallocation for you, in case you wanted to add this to other DJs.
+  void DJ::AddSong(AudioFile *toAdd)
   {
-
+    audioSystem_.PreloadFile(*toAdd);
+    playlist_.Add(toAdd);
   }
+
 
   // Fills the array provided with the active spectrum.
   void DJ::FillSongSpectrum(float* toFill, AudioDataSize size, FMOD_DSP_FFT_WINDOW window)
   {
-    audioSystem_.FillSpectrum(toFill, size, 0, window)
+    audioSystem_.FillSpectrum(toFill, size, 0, window);
+  }
+
+
+    //////////////////////////////
+   // Private member funcitons //
+  //////////////////////////////
+  void DJ::playlistUpdatedCallback()
+  {
+    // Stop the song and play a new one, since we've been updated.
+    if(currSong_)
+      audioSystem_.StopFile(*currSong_);
+
+    currSong_ = playlist_.GetCurrent();
+    audioSystem_.PlayFile(*currSong_);
   }
 }
--- Source/DJ.hpp
+++ Source/DJ.hpp
@@ -1,7 +1,7 @@
 //Custom DJ class for managing the Playlist, SoundSystem, and Visualizer
 #pragma once
 #include "AudioSystem.hpp"
-#include "CustomDefines.hpp"
+#include "Defines.hpp"
 #include "Playlist.hpp"
 #include "Visualizer.hpp"
 #include "DefaultVisualizer.hpp"
@@ -15,24 +15,31 @@ namespace ASCIIPlayer
   {
   public:
     // Constructor
-    DJ(DJConfig config);
+    DJ(DJConfig config = DJConfig(), bool startPaused = false);
     ~DJ();
 
     // Member Functons
     bool Update();
+    void Play();
+    void Pause();
     void Shutdown();
     void ShufflePlaylist();
-    void AddSong(AudioFile toAdd);
+    void AddSong(AudioFile *toAdd);
     void FillSongSpectrum(float* toFill, AudioDataSize size, FMOD_DSP_FFT_WINDOW window);
 
   private:
+    // Callback
+    void playlistUpdatedCallback();
+
     // Variables
-    Playlist playlist_;             // Contains the AudioFile objects.
+    Playlist<DJ> playlist_;         // Contains the AudioFile objects.
     AudioSystem audioSystem_;       // The sound system to play the audio files.
     Visualizer *visualizer_;        // The visualizer to display the playing data.
     DJConfig config_;               // The proivded config for the DJ.
 
     // Internal Tracking
-    bool hasShutdown_;
+    bool hasShutdown_;  // Whether or not we have shut down.
+    bool paused_;
+    AudioFile *currSong_;
   };
 }
