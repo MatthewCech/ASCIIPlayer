5ca28b62e034b00fb8e222529b5205cfbfd48488
GHOST\rever <reveriewisp@gmail.com>
2016-11-06 00:44:43 -0700

    + Added copy batch file for post build

---
+++ UpdateASCIIExistingForRev.bat
@@ -0,0 +1,9 @@
+@echo off
+rem Terrible hardcoded batch file for moving the file I use to update my working version of ASCIIPlayer on just my machines.
+rem You will inevitably forget that this favors the release build.
+
+copy bin_x86_Debug_vs2015\ascii_player.exe C:\Users\rever\OneDrive\Programs\Expanded\ASCIIPlayer\ascii_player.exe
+copy bin_x86_Debug_vs2015\fmodexL.dll C:\Users\rever\OneDrive\Programs\Expanded\ASCIIPlayer\fmodexL.dll
+copy bin_x86_Release_vs2015\fmodex.dll C:\Users\rever\OneDrive\Programs\Expanded\ASCIIPlayer\fmodex.dll
+copy bin_x86_Release_vs2015\ascii_player.exe C:\Users\rever\OneDrive\Programs\Expanded\ASCIIPlayer\ascii_player.exe
+timeout 5
