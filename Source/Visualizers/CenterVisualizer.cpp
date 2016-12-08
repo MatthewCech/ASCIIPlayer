2230a4c6ea0d2c9acfaae7062b58960349de818c
GHOST\rever <reveriewisp@gmail.com>
2016-12-08 02:32:56 -0800

    + Added central diamond implementation

--- Source/Visualizers/CenterVisualizer.cpp
+++ Source/Visualizers/CenterVisualizer.cpp
@@ -9,9 +9,9 @@
 
 namespace ASCIIPlayer
 {
-  // Ctor
+  // Ctor - hide the cursor and set up.
   CenterVisualizer::CenterVisualizer() 
-    : Visualizer(128, aSpectrum, "CenterVisualizer")
+    : Visualizer(64, aSpectrum, "centerVisualizer")
     , lastWidth_(CONSOLE_WIDTH_FUNC)
     , lastHeight_(CONSOLE_HEIGHT_FUNC)
   {  
@@ -22,38 +22,72 @@ namespace ASCIIPlayer
   bool CenterVisualizer::Update(float* data)
   {
     // Variables
-    float scalar = 800;                              // Amplitude Scalar
-    int audioDataWidth = GetAudioDataSize() / 2 - 1; // Audio data width we use
-    int width = CONSOLE_WIDTH_FUNC;                  // Console Width
-    int height = CONSOLE_HEIGHT_FUNC;                // Console Height
-    int halfHeight = height / 2;                     // Vertical Offset
-    int centerOffset = (width - audioDataWidth) / 2; // Horizontal Offset
-    if (centerOffset < 0)
-      centerOffset = 0;
+    float scalar{ 800 };               // Amplitude Scalar
+    int width{ CONSOLE_WIDTH_FUNC };   // Console Width
+    int height{ CONSOLE_HEIGHT_FUNC }; // Console Height
 
     // Re-init if needed
     if (lastHeight_ != height || lastWidth_ != width)
     {
       RConsole::Canvas::ReInit(width, height);
       rlutil::cls();
+      lastHeight_ = height;
+      lastWidth_ = width;
+      return false;
     }
+
+    // Calculate dependant values and precompute values.
+    int audioDataWidth{ static_cast<int>(GetAudioDataSize() / 3 - 1) };                      // Audio data width we use
+    int centerOffsetH = (width - audioDataWidth) / 2 < 0 ? 0 : (width - audioDataWidth) / 2; // Horizontal Offset
+    int halfHeight{ height / 2 };                                                            // Vertical Offset
+    int halfDataWidth{ (audioDataWidth / 2) };                                               // Horizontal Offset
 
     // Display spectrum bar in center
     for (int i = 0; i < audioDataWidth; ++i)
     {
-      for (unsigned int j = 0; j < log(data[i] * scalar); ++j)
+      // Ignore strange edge case values and negatives.
+      if (data[i] < .001) continue;
+
+      // Calculate the log of the input scaled up to make a more smooth visualizer.
+      // If it is under 0 for some reason, ignore it.
+      float logH{ abs(log(data[i] * scalar)) };
+      int actualWidth{ static_cast<int>(logH - (static_cast<float>(i) / logH)) };
+      if (actualWidth <= 0) continue;
+
+      // Place 4 mirrored blocks.
+      for (int j = 0; j < actualWidth; ++j)
       {
+        // Lower Left quadrant
         RConsole::Canvas::Draw(
           static_cast<unsigned char>(219)
-          , static_cast<float>(centerOffset + i)
-          , static_cast<float>(j)
+          , static_cast<float>((centerOffsetH - i) + halfDataWidth)
+          , static_cast<float>(j + halfHeight)
+          , RConsole::YELLOW);
+
+        // Lower right quadrant
+        RConsole::Canvas::Draw(
+          static_cast<unsigned char>(219)
+          , static_cast<float>((centerOffsetH + i) + halfDataWidth)
+          , static_cast<float>(j + halfHeight)
+          , RConsole::YELLOW);
+
+        // Upper left quadrant
+        RConsole::Canvas::Draw(
+          static_cast<unsigned char>(219)
+          , static_cast<float>((centerOffsetH + audioDataWidth - i) - halfDataWidth)
+          , static_cast<float>(-j + halfHeight)
+          , RConsole::YELLOW);
+
+        // Upper right quadrant
+        RConsole::Canvas::Draw(
+          static_cast<unsigned char>(219)
+          , static_cast<float>((centerOffsetH + audioDataWidth + i) - halfDataWidth)
+          , static_cast<float>(-j + halfHeight)
           , RConsole::YELLOW);
       }
     }
 
-
-    lastHeight_ = height;
-    lastWidth_ = width;
+    // Update the canvas
     return RConsole::Canvas::Update();
   }
 }
