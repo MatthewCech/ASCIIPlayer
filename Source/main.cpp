3891b3bcf892a7fb8cc9994e7115978396985467
Matthew Cech <contact@matthewcech.com>
2019-02-15 01:17:55 -0800

    + Added shoom and implemented opening when selecting

--- Source/main.cpp
+++ Source/main.cpp
@@ -4,19 +4,27 @@
 
 
 
+namespace ASCIIPlayer
+{
+  int Run(int argc, char** argv)
+  {
+    // Stop logging
+    FMOD::Debug_SetLevel(FMOD_DEBUG_LEVEL_NONE);
+
+    // Run program
+    ASCIIPlayer::Lobby lounge(argc, argv);
+    lounge.Run();
+
+    // Return normally
+    return 0;
+  }
+}
+
+// Run main, catching dumping any errors.
 int main(int argc, char** argv) try
 {
-  // Stop logging
-  FMOD::Debug_SetLevel(FMOD_DEBUG_LEVEL_NONE);
-
-  // Run program
-  ASCIIPlayer::Lobby lounge(argc, argv);
-  lounge.Run();
-
-  // Return normally
-  return 0;
+  return ASCIIPlayer::Run(argc, argv);
 }
-//Catch and dump any errors.
 catch (std::exception &e)
 {
   std::cerr << e.what() << std::endl;
