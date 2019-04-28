4fdea83d4fb468f22f2f4627b0cb6c319f7147ea
Matthew Cech <contact@matthewcech.com>
2019-04-28 00:55:46 -0700

    + Added notes and installs

--- _build_linux.sh
+++ _build_linux.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#Update and upgrade the system, verify gcc build essentials is installed. The primary linux limitation is that the library files are 32-bit only, and can only be compiled with -m32 / -melf_i386 compatibility settings. Adding --verbose to `ld` seems to help with debugging.
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install build-essential
+sudo apt-get -y install ia32-libs
 
 Build/run_clean.sh
 Build/run_premake_gmake.sh
