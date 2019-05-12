6971d8240360996399ae31ada118cb316447db98
Matthew Cech <contact@matthewcech.com>
2019-05-11 21:44:20 -0700

    + added linux libs for 1.10.13, x86/x64/arm

--- CompileTest/go.sh
+++ CompileTest/go.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install build-essential
-sudo apt-get -y install ia32-libs
+#sudo apt-get -y update
+#sudo apt-get -y upgrade
+#sudo apt-get -y install build-essential
+#sudo apt-get -y install ia32-libs
 
 echo ""
 echo " --- [Compiling and Linking] ---"
@@ -12,4 +12,4 @@ echo ""
 #This fails b/c 32 vs 64 bit:
 # g++ -I. -L. -lfmodex main.cpp
 
-g++ -m32 -I. -L. -lfmodex main.cpp
+g++ -pthread -m32 main.cpp -L./lib/x86 -lfmodL -I./inc -o output
