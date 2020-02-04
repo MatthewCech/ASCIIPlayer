e0675eff46893b4192841c5e9a1ac9c6650c0744
Matthew Cech <contact@matthewcech.com>
2020-02-03 23:11:51 -0800

    = Update to VS2019

--- _build_win.bat
+++ _build_win.bat
@@ -1,5 +1,6 @@
 @echo off
-rem This is just a one-stop file to run the vs2017 build script. Cleans then builds it again, but doesn't work if the files are in use.
+rem This is just a one-stop file to run the vs2019 build script. Cleans then builds it again, but doesn't work if the files are in use.
+rem If there is another build format that's desired, check out the build folder.
 
 echo -- [ CLEANING ] --
 cd Build
@@ -10,16 +11,16 @@ echo.
 echo -- [ BUILDING ] --
 cd Build
 echo Building...
-call build_vs2017.bat
+call build_vs2019.bat
 
 echo.
 echo.
 echo -- [ MOVING RESOURCES ] --
 cd ..
-copy Resources project_vs2017
-copy Resources\icon project_vs2017
-copy Resources\dll project_vs2017
-copy Resources\samples project_vs2017
+copy Resources project_vs2019
+copy Resources\icon project_vs2019
+copy Resources\dll project_vs2019
+copy Resources\samples project_vs2019
 
 echo.
 echo.
